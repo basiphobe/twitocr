b64c0c893873763783a7095a1375c3de7b3cacc7
Aaron Smith <basiphobe@gmail.com>
2018-06-20 16:16:42 -0400

    Adding generic configs.

--- config.js
+++ config.js
@@ -1,8 +1,12 @@
 const options = {
-    consumer_key: 'SfphByokwXAWw34FX0funQ4RK',
-    consumer_secret: 'ye9rWYB29kTYgXqhuNhgotRxqxPIaLm8b8sN9fVUCG7YU7xcM2',
-    access_token_key: '214080741-W3usHFNdyq5oZ9XoHtb7RMz3pgmWZVkcYbIRf0f6',
-    access_token_secret: 'ZxZ81SuZNLkqMFSNas5cVJ0ZyeIr65BIz6AEC6fL2CKix'
-}
+    consumer_key: 'XXXXXXXXXXXXXXXXXXXXXXXX',
+    consumer_secret: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    access_token_key: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    access_token_secret: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    ocr_apikey: 'xxxxxxxxxxxxxxxxxxxxx',
+    server_port: 7000,
+    twitterAPIHost: 'https://api.twitter.com/1.1',
+    ocrHashTag: 'ocrme'
+};
 
 module.exports = options;
