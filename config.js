b49af215cb8b18e3811b39b3cf78fa3fcbb3dacb
Aaron Smith <basiphobe@gmail.com>
2018-07-02 13:01:26 -0400

    Updating config

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 const options = {
-    consumer_key: 'XXXXXXXXXXXXXXXXXXXXXXXX',
-    consumer_secret: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    access_token_key: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    access_token_secret: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    ocr_apikey: 'xxxxxxxxxxxxxxxxxxxxx',
+    behindProxy: true,
+    consumer_key: 'xxxxxxxxxxxxxxxxxxxxxxxx',
+    consumer_secret: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    access_token_key: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    access_token_secret: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    ocr_apikey: 'xxxxxxxxxxxxxxxxx',
     server_port: 7000,
     twitterAPIHost: 'https://api.twitter.com/1.1',
     ocrHashTag: 'ocrme'
