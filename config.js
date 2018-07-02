4f9397e9d788da58747da805a1db641db033e815
Aaron Smith <basiphobe@gmail.com>
2018-07-02 13:03:26 -0400

    Updating config (without conflicts this time)

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 const options = {
     behindProxy: true,
-<<<<<<< HEAD
     consumer_key: 'xxxxxxxxxxxxxxxxxxxxxxxx',
     consumer_secret: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
     access_token_key: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
@@ -9,16 +8,6 @@ const options = {
     server_port: 7000,
     twitterAPIHost: 'https://api.twitter.com/1.1',
     ocrHashTag: 'ocrme'
-=======
-    server_port: 7000,
-    twitterAPIHost: 'https://api.twitter.com/1.1',
-    ocrHashTag: 'ocrme',
-    consumer_key: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    consumer_secret: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    access_token_key: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    access_token_secret: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    ocr_apikey: 'xxxxxxxxxxxxxxxxxxxxx',
->>>>>>> e10bb5f218d2433fb65504cefe01eba0accd43f8
 };
 
 module.exports = options;
